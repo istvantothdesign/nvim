84e5a053e8aa30c0138d013b96b421f39238327c
istvantothdesign <69763751+istvantothdesign@users.noreply.github.com>
2024-01-31 19:29:06 +0000

    Plugin updates.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.api.nvim_set_keymap("n", "<leader>p", [[:set paste!<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>W", [[:set wrap! linebreak!<CR>]], { noremap = true, silent = true })
