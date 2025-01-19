2d623547f8c48f4a0832e683de64a012d1828498
istvantothdesign <69763751+istvantothdesign@users.noreply.github.com>
2025-01-19 13:58:08 +0000

    Added Codeium and fixed issue with tab shortcut ⌨️

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,17 +1,26 @@
--- Remove the `use` here if you're using folke/lazy.nvim.
 return {
   "Exafunction/codeium.vim",
   config = function()
-    -- Change '<C-g>' here to any keycode you like.
-    vim.keymap.set("i", "<C-g>", function()
+    -- vim.keymap.set("i", "<Tab>", function()
+    --   return vim.fn["codeium#Accept"]() ~= "" and vim.fn["codeium#Accept"]() or "<Tab>"
+    -- end, { expr = true, silent = true })
+
+    -- Set the fallback for <Tab>
+    vim.g.codeium_tab_fallback = "<Tab>"
+
+    -- Map <Tab> to accept Codeium suggestions
+    vim.keymap.set("i", "<Tab>", function()
       return vim.fn["codeium#Accept"]()
     end, { expr = true, silent = true })
+
     vim.keymap.set("i", "<c-;>", function()
-      return vim.fn["codeium#CycleCompletions"](1)
+      return vim.fn
     end, { expr = true, silent = true })
+
     vim.keymap.set("i", "<c-,>", function()
       return vim.fn["codeium#CycleCompletions"](-1)
     end, { expr = true, silent = true })
+
     vim.keymap.set("i", "<c-x>", function()
       return vim.fn["codeium#Clear"]()
     end, { expr = true, silent = true })
